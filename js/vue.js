e7724e8b043ce2a6d3f153b421edabbc60c2d85e
ShigekiKozawa <sigekikozawa@aol.com>
2019-10-30 17:47:07 +0900

    初稿：axiosで取得したデータを変数に格納できずメソッドが通せない笑、取得したデータを直接表示

--- js/vue.js
+++ js/vue.js
@@ -1,11 +1,11 @@
 let vm = new Vue({
   el: '#app',
   data: {
-    articles: []
+    results: []
   },
   mounted :function(){
     axios.get('https://newsapi.org/v2/top-headlines?country=jp&apiKey=f811a838a1134a3abc0190bffdc597d2')
-          .then(response => this.articles = response.data)
+          .then(response => this.results = response.data.articles)
           .catch(response => console.log(response))
   }
 });

f141c6d792fb94dd45bc93998f3c682a6ba33c05
ShigekiKozawa <sigekikozawa@aol.com>
2019-11-01 16:17:08 +0900

    セレクタを設置してカテゴリ別に表示できるようにした

--- js/vue.js
+++ js/vue.js
@@ -1,11 +1,27 @@
-let vm = new Vue({
-  el: '#app',
-  data: {
-    results: []
-  },
-  mounted :function(){
-    axios.get('https://newsapi.org/v2/top-headlines?country=jp&apiKey=f811a838a1134a3abc0190bffdc597d2')
-          .then(response => this.results = response.data.articles)
-          .catch(response => console.log(response))
-  }
+const category = "general, business, entertainment, health, science, sports, technology";
+const url = 'https://newsapi.org/v2/';
+const country = 'jp';
+const key = 'f811a838a1134a3abc0190bffdc597d2';
+const vm = new Vue({
+    el: '#app',
+    data: {
+        results: [],
+        sections: category.split(', '),
+        section: 'general'
+    },
+    mounted :function(){
+        this.getPosts('general');
+    },
+    methods: {
+        getPosts(section) {
+            axios.get(
+                url + 'top-headlines' + 
+                '?country=' + country + 
+                '&category=' + this.section +
+                '&apiKey=' + key
+            )
+                .then(response => this.results = response.data.articles)
+                .catch(response => console.log(response))
+        }
+    }
 });
